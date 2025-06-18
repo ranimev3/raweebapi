fb5c3c4341711ee91e7c7aa6d5fd436cc5e3bd1e
ranimev3 <coinmana30@gmail.com>
2025-06-18 16:44:09 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import apiRoutes from './routes/apiRoutes.js';
+import router from './routesv2/router.js
 
 const app = express();
 const PORT = process.env.PORT || 3000;
