4429da67299e25651021144459bbd38154a0a9b0
ranimev3 <coinmana30@gmail.com>
2025-06-22 11:07:01 +0700

    Update fetchPage.js

--- src/utils/fetchPage.js
+++ src/utils/fetchPage.js
@@ -16,8 +16,8 @@ const headers = {
   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
   "Accept-Encoding": "gzip, deflate, br",
   "Accept-Language": "en-US,en;q=0.9,id;q=0.8",
-  "Referer": "https://id.manhwaindo.cx/",
-  "Origin": "https://id.manhwaindo.cx",
+  "Referer": "https://manhwaindo.com/",
+  "Origin": "https://manhwaindo.com",
   "X-Requested-With": "XMLHttpRequest",  
   "Upgrade-Insecure-Requests": "1",  
 };
