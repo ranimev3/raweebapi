06bc7fe5c747e60f24234a5e3b28fbf51b617b90
ranimev3 <coinmana30@gmail.com>
2025-06-26 10:43:42 +0700

    Update scrapingController.js

--- src/controllers/scrapingController.js
+++ src/controllers/scrapingController.js
@@ -466,8 +466,8 @@ export const getManhwaDetail = async (req, res) => {
       .text()
       .trim();
 
-    const status = $(".tsinfo .bixbox").eq(0).find("i").text().trim();
-    const type = $(".tsinfo .bixbox").eq(1).find("a").text().trim();
+    const status = $(".imptdt").eq(0).find("i").text().trim();
+    const type = $(".imptdt").eq(1).find("a").text().trim();
     const released = $(".imptdt").eq(0).find("span").text().trim();
     const author = $(".imptdt").eq(1).find("span").text().trim();
     const artist = $(".imptdt").eq(2).find("span").text().trim();
