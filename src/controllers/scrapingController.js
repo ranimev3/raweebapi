f1f04dd9bcf3e84fa9895b726e2d736a865812ce
ranimev3 <coinmana30@gmail.com>
2025-06-26 10:22:40 +0700

    Update scrapingController.js

--- src/controllers/scrapingController.js
+++ src/controllers/scrapingController.js
@@ -474,7 +474,7 @@ export const getManhwaDetail = async (req, res) => {
     const updatedOn = $(".fmed").find("span time").text().trim();
     
     const genres = [];
-    $(".seriesgenre").each((index, element) => {
+    $(".seriestugenres").each((index, element) => {
       const genreName = $(element).text().trim();
       const genreLink = $(element).attr("href");
       genres.push({
