ef33c44012f89728191a7e4a9bd7fa1e302521a2
ranimev3 <coinmana30@gmail.com>
2025-07-26 09:24:38 +0700

    Update apiRoutes.js

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -13,7 +13,21 @@ import {
   getManhwaDetail,
   getManhwaOnGoing,
   getChapter,
-  getList
+  getList,
+  getHomev2,
+  getManhwaPopularv2,
+  getManhwaRecommendationv2,
+  getManhwaNewv2,
+  getManhwaTopv2,
+  getGenresv2,
+  getGenreIdv2,
+  getGenreIdPagev2,
+  getSearchv2,
+  getSearchPagev2,
+  getManhwaDetailv2,
+  getManhwaOnGoingv2,
+  getChapterv2,
+  getListv2
 } from "../controllers/scrapingController.js";
 
 const router = express.Router();
@@ -31,6 +45,20 @@ router.get("/search/:searchId/page/:pageNumber", getSearchPage);
 router.get("/manhwa-detail/:manhwaId", getManhwaDetail);
 router.get("/manhwa-ongoing", getManhwaOnGoing);
 router.get("/chapter/:chapterId", getChapter); 
-router.get("/list", getList); 
+router.get("/list", getList);
+router.get("/komikststion/home", getHomev2);
+router.get("/komikstation/manhwa-popular", getManhwaPopular);
+router.get("/komikstation/manhwa-recommendation", getManhwaRecommendation);
+router.get("/komikstation/manhwa-new", getManhwaNew);
+router.get("/komikstation/manhwa-top", getManhwaTop);
+router.get("/komikstation/genres", getGenres);
+router.get("/komikstation/genre/:genreId", getGenreId);
+router.get("/komikstation/genre/:genreId/page/:pageNumber", getGenreIdPage);
+router.get("/komiktation/search/:searchId", getSearch);
+router.get("/komikstation/search/:searchId/page/:pageNumber", getSearchPage);
+router.get("/komikstation/manhwa-detail/:manhwaId", getManhwaDetail);
+router.get("/komikstation/manhwa-ongoing", getManhwaOnGoing);
+router.get("/komikstation/chapter/:chapterId", getChapter); 
+router.get("/komikstation/list", getListv2);
 
 export default router;
