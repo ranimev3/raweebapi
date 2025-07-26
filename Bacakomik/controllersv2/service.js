c7fdf9a1309168fc57598e0f58d764bf756a0531
ranimev3 <coinmana30@gmail.com>
2025-07-26 17:04:08 +0700

    Update domain

--- Bacakomik/controllersv2/service.js
+++ Bacakomik/controllersv2/service.js
@@ -2,8 +2,8 @@ import axios from "axios"
 
 const headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
-    "Referer": "https://bacakomik.one/",
-    "Origin": "https://bacakomik.one",
+    "Referer": "https://bacakomik.my/",
+    "Origin": "https://bacakomik.my",
 }
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
